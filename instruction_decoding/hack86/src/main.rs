5651a45e727072b91d807320c7b94b1065864994
Simon Morgan <sjm@sjm.io>
2023-07-15 22:59:23 +0100

    Add basic command-line parsing

--- instruction_decoding/hack86/src/main.rs
+++ instruction_decoding/hack86/src/main.rs
@@ -1,10 +1,32 @@
 mod decode;
 
 fn main() {
-    let args: Vec<String> = std::env::args().skip(1).collect();
-    let path = args.first().expect("failed to get path to binary");
+    let mut simulate: bool = false;
 
-    let instruction_stream = std::fs::read(path).expect("failed to read file");
+    let args: Vec<String> = std::env::args().skip(1).collect();
+    if let Some(first) = args.first() {
+        let path = if first == "-s" {
+            simulate = true;
+
+            args.iter().nth(1)
+        } else {
+            args.first()
+        };
+
+        if let Some(path) = path {
+            let instruction_stream = std::fs::read(path).expect("failed to read file");
+            if simulate {
+                simulation(&instruction_stream);
+            } else {
+                decode(&instruction_stream);
+            }
+        }
+    } else {
+        eprintln!("Please specify the path to a binary.");
+    }
+}
+
+fn decode(instruction_stream: &[u8]) {
     let mut instruction_index = 0;
     while instruction_index < instruction_stream.len() {
         if let Ok(instruction) =
@@ -19,4 +41,8 @@ fn main() {
             );
         }
     }
+}
+
+fn simulation(instruction_stream: &[u8]) {
+    eprintln!("Simulation not implemented yet!")
 }
